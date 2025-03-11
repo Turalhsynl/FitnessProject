8da80fb18f984616df9ebaef907f24da6d1765c7
Tural.Dev <tural.huseynli.2005@gmail.com>
2025-03-11 22:28:13 +0400

    validators added

---
+++ Application/CQRS/Users/Validators/RegisterValidators.cs
@@ -0,0 +1,36 @@
+﻿using Application.CQRS.Users.Handlers;
+using FluentValidation;
+
+namespace Application.CQRS.Users.Validators;
+
+public class RegisterValidators : AbstractValidator<Register.Command>
+{
+    public RegisterValidators()
+    {
+        RuleFor(u => u.Firstname)
+            .NotEmpty()
+            .MaximumLength(255);
+
+        RuleFor(u => u.Lastname)
+            .NotEmpty()
+            .MaximumLength(255);
+
+        RuleFor(u => u.Age)
+            .NotEmpty()
+            .GreaterThan(18);
+
+        RuleFor(u => u.Gender)
+            .NotEmpty()
+            .MaximumLength(20);
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .MaximumLength(100)
+            .EmailAddress();
+
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(50);
+    }
+}
