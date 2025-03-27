9bd0f785d45e518b4f18705501f571cc859ea4d5
USER <USER@DESKTOP-JI95IOS>
2025-03-27 13:13:34 +0400

    User Entity updated

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -7,7 +7,7 @@ public class User : BaseEntity
 {
     public string Firstname { get; set; }
     public string Lastname { get; set; }
-    public string Gender { get; set; }
+    public UserGender Gender { get; set; }
     public int Age { get; set; }
     public Decimal Height { get; set; }
     public Decimal Weight { get; set; }
---
+++ Domain/Enums/UserGender.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Enums;
+
+public enum UserGender : int
+{
+    Male=1,
+    Female=2,
+    Other=3,
+}
